7880468b3b6550ca42472c19ed5673740d4b89bf
PrinceFPF <1159739336@qq.com>
2024-06-27 09:02:37 +0800

    Update update_dedecms.py

--- scripts/update_dedecms.py
+++ scripts/update_dedecms.py
@@ -5,7 +5,7 @@ from github import Github
 from bs4 import BeautifulSoup
 
 # 配置信息
-GITHUB_TOKEN = os.environ['MY_API_TOKEN']
+GITHUB_TOKEN = os.environ['my_api_token']
 REPO_NAME = 'DedeCMS'
 OWNER = 'PrinceFPF'
 
